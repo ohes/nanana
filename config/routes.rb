369b9d9512998917fc19a880110152d85b539a91
ohes <estheroh@gmail.com>
2015-04-30 02:51:38 +0000

    initial bootstrap install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Splurty::Application.routes.draw do
 
-  root 'quotes#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +55,9 @@ Splurty::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+root 'quotes#index'
+
 end
+
+
