d9a0c991ce94eb12624dc6cbbe3ef89fa7b2a886
ohes <estheroh@gmail.com>
2015-05-04 03:08:49 +0000

    built the about page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -14,8 +14,10 @@ class QuotesController < ApplicationController
 		if @quote.invalid?
 			flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
 		end
-
 		redirect_to root_path
+	end
+
+	def about
 	end
 
 	private
@@ -23,5 +25,4 @@ class QuotesController < ApplicationController
 	def quote_params
 		params.require(:quote).permit(:saying, :author)
 	end
-
 end
